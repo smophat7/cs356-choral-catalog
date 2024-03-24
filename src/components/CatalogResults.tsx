773dcc2f2d2cd14a37d6b61135839ed4a84f72c6
Spencer M <spencer.g.moffat@gmail.com>
2024-03-24 17:11:01 -0600

    Fix grid/flex on list/detail catalog view

--- src/components/CatalogResults.tsx
+++ src/components/CatalogResults.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { Box, Flex, Grid, NavLink, ScrollArea, Stack } from "@mantine/core";
+import { Box, Flex, NavLink, ScrollArea, Stack } from "@mantine/core";
 
 import { Song } from "../types";
 import SongDetails from "./SongDetails";
@@ -19,32 +19,28 @@ const CatalogResults: React.FC<Props> = ({ songs }) => {
 
   return (
     <Box flex={1} style={{ overflow: "hidden" }}>
-      <Grid h="100%">
-        <Grid.Col span={{ base: 4, md: 2 }}>
-          <ScrollArea>
-            <Stack gap={0}>
-              {songs && songs.length > 0 && (
-                <>
-                  {songs.map((song, index) => (
-                    <NavLink
-                      key={index}
-                      onClick={() => setSelectedSong(song)}
-                      active={selectedSong === song}
-                      label={getLabel(song)}
-                      description={getDescription(song)}
-                    />
-                  ))}
-                </>
-              )}
-            </Stack>
-          </ScrollArea>
-        </Grid.Col>
-        <Grid.Col span={{ base: 8, md: 10 }}>
-          <ScrollArea w="100%" pl="md" pt="md">
-            {selectedSong && <SongDetails song={selectedSong} />}
-          </ScrollArea>
-        </Grid.Col>
-      </Grid>
+      <Flex h="100%">
+        <ScrollArea h="100%" w="25%">
+          <Stack gap={0}>
+            {songs && songs.length > 0 && (
+              <>
+                {songs.map((song, index) => (
+                  <NavLink
+                    key={index}
+                    onClick={() => setSelectedSong(song)}
+                    active={selectedSong === song}
+                    label={getLabel(song)}
+                    description={getDescription(song)}
+                  />
+                ))}
+              </>
+            )}
+          </Stack>
+        </ScrollArea>
+        <ScrollArea w="100%" pl="md" pt="md">
+          {selectedSong && <SongDetails song={selectedSong} />}
+        </ScrollArea>
+      </Flex>
     </Box>
   );
 };
