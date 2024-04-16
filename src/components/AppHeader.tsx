b2dec10294c2dc1248d9c2330d7e874294cb4cee
Spencer M <spencer.g.moffat@gmail.com>
2024-04-16 17:09:45 -0600

    Welcome message spacing. Add icon to header.

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,12 +1,16 @@
 import { AppShell, Box, Container, Group, Text, Title } from "@mantine/core";
+import { IconMusic } from "@tabler/icons-react";
 
 const AppHeader: React.FC = () => {
   return (
     <AppShell.Header>
       <Container h="100%" size="xl">
-        <Group h="100%" gap="sm" justify="space-between">
+        <Group h="100%" gap="sm" justify="flex-start">
           <Box>
-            <Title order={2}>Choral Catalog</Title>
+            <Group justify="space-between" gap={0}>
+              <IconMusic />
+              <Title order={2}>Choral Catalog</Title>
+            </Group>
             <Text c="dimmed" size="xs">
               Find the perfect songs for your choir
             </Text>
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Button, Center, List, Stack, Text, Title } from "@mantine/core";
+import { Box, Button, Center, List, Stack, Text, Title } from "@mantine/core";
 
 interface Props {
   close: () => void;
@@ -14,14 +14,17 @@ const Welcome: React.FC<Props> = ({ close }) => {
         Find the perfect piece for your choir with our extensive collection of
         choral sheet music.
       </Text>
-      <>To enhance your browsing experience, we offer:</>
-      <List withPadding>
-        <List.Item>Saved search filters</List.Item>
-        <List.Item>Voicing details and vocal ranges</List.Item>
-        <List.Item>Sheet music previews</List.Item>
-        <List.Item>Audio and video recordings</List.Item>
-        <List.Item>Direct links to purchase</List.Item>
-      </List>
+      <Box>
+        <Text>To enhance your browsing experience, we offer:</Text>
+
+        <List withPadding>
+          <List.Item>Saved search filters</List.Item>
+          <List.Item>Voicing details and vocal ranges</List.Item>
+          <List.Item>Sheet music previews</List.Item>
+          <List.Item>Audio and video recordings</List.Item>
+          <List.Item>Direct links to purchase</List.Item>
+        </List>
+      </Box>
       <Center>
         <Button onClick={close} size="lg">
           Let's Go!
